69146ca3b701745b3991a52b0c26ff2bb83a5523
adriaanboon <71772080+adriaanboon@users.noreply.github.com>
2020-10-22 14:44:22 +0200

    Autotest opdracht 1,2 en 3.

--- src/Auto.java
+++ src/Auto.java
@@ -1,2 +1,46 @@
 public class Auto {
+    int snelheid =0;
+    int kilometerstand =23000;
+    private String merk="Toyota";
+
+    public static void main(String[] args) {
+        Auto auto1= new Auto();
+        auto1.start();
+        auto1.stop();
+        auto1.geefGas();
+        System.out.println(auto1.getKilometerStand());
+    }
+
+    void start() {
+        System.out.println("De " +merk+  " start.");
+    }
+    void stop() {
+        System.out.println("De " +merk+ " stopt.");
+    }
+    public void geefGas() {
+        System.out.print("De " +merk+ " rijdt ");
+        snelheid+=20;
+        kilometerstand+=20;
+        System.out.print(snelheid);
+        System.out.println(" en heeft in totaal "+kilometerstand+ " gereden.");
+    }
+    public String getMerk() {
+        return merk;
+    }
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+    int getKilometerStand() {
+        System.out.print("De kilometerstand van de " +merk+ " is: ");
+        return kilometerstand;
+    }
+    void setKilometerstand(int kilometerstand) {
+        this.kilometerstand=kilometerstand;
+    }
+    int getSnelheid() {
+        return snelheid;
+    }
+    void setSnelheid(int snelheid) {
+        this.snelheid=snelheid;
+    }
 }
---
+++ src/Autotest.java
@@ -0,0 +1,17 @@
+public class Autotest {
+
+    public static void main(String[] args) {
+        Auto auto2= new Auto();
+        auto2.setKilometerstand(50000);
+        auto2.start();
+        auto2.geefGas();
+        auto2.stop();
+        System.out.println(auto2.getKilometerStand());
+        Auto auto3= new Auto();
+        auto3.setMerk("Volvo");
+        auto3.setKilometerstand(10000);
+        auto3.start();
+        auto3.geefGas();
+        auto3.geefGas();
+    }
+}
